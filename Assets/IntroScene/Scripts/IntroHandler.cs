8a6a559a9e6b810b838e6bbdda35a8cfc5338384
Michael Perez <mperez980089@yahoo.com>
2019-01-26 01:15:58 -0800

    Imported fungus, set up intro and witch scene

---
+++ Assets/IntroScene/Scripts/IntroHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroHandler : MonoBehaviour
+{
+    public Image fadeOverlay;
+    public Sprite still2;
+    public SpriteRenderer sr;
+
+    void Start()
+    {
+        LeanTween.value(1, 0, 2).setOnUpdate((float value) =>
+        {
+            fadeOverlay.color = new Color(fadeOverlay.color.r, fadeOverlay.color.g, fadeOverlay.color.b, value);
+        }).setOnComplete(() =>
+        {
+            StartCoroutine(IntroSequence());
+        });
+    }
+
+    IEnumerator IntroSequence()
+    {
+        yield return new WaitForSeconds(2);
+        sr.sprite = still2;
+        yield return new WaitForSeconds(2);
+
+        yield return null;
+    }
+}
