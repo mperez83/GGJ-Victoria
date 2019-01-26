0c6d97a9b2b3f8465ff568d10546d9e6ec913c07
Michael Perez <mperez980089@yahoo.com>
2019-01-25 23:30:30 -0800

    Imported TextMeshPro, added basic main menu, incorporated (broken) fadeout on hitting play

---
+++ Assets/MainMenu/Scripts/MainMenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public Image fadeOutOverlay;
+
+    public void StartGame()
+    {
+        LeanTween.value(0, 1, 2).setOnUpdate((float value) =>
+        {
+            fadeOutOverlay.color = new Color(fadeOutOverlay.color.r, fadeOutOverlay.color.g, fadeOutOverlay.color.b, value);
+        }).setOnComplete(() =>
+        {
+            SceneManager.LoadScene("IntroScene");
+        });
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
