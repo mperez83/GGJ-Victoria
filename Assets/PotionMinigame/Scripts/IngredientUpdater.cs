45824f43f8d6a6a07a47b48970dea7a6927188c3
Michael-C-Kirk <michael_c_kirk@yahoo.com>
2019-01-26 00:52:30 -0800

    Basic Potion Mini Game Mechanics
    +Scripts: Player Controller, Ingredient Spawner, Ingredient Trigger, Ingredient Updater
    +Placeholder sprites
    +A couple Prefabs

---
+++ Assets/PotionMinigame/Scripts/IngredientUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientUpdater : MonoBehaviour
+{
+    Image m_Image;
+    public Sprite m_sprite1;
+    public Sprite m_sprite2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("ChangeImage", .5f, .5f);
+    }
+
+    void ChangeImage()
+    {
+        Debug.Log(m_Image.sprite);
+        if (m_Image.sprite = m_sprite2)
+        {
+            Debug.Log("sprite2");
+            m_Image.sprite = m_sprite1;
+        }
+        else if (m_Image.sprite = m_sprite1)
+        {
+            Debug.Log("sprite1");
+            m_Image.sprite = m_sprite2;
+        }
+    }
+}
---
+++ Assets/PotionMinigame/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject blue;
+    public GameObject smile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn1());
+    }
+
+    IEnumerator Spawn1()
+    {
+        yield return new WaitForSeconds(2);
+        Instantiate(blue, new Vector3(5, 0, 0), Quaternion.identity);
+        StartCoroutine(Spawn2());
+    }
+
+    IEnumerator Spawn2()
+    {
+        yield return new WaitForSeconds(2);
+        Instantiate(smile, new Vector3(-5, 0, 0), Quaternion.identity);
+        StartCoroutine(Spawn1());
+    }
+
+}
