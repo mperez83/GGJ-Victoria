ab9537592175c252517170581d0ac80960d10cbb
Michael Perez <mperez980089@yahoo.com>
2019-01-27 10:23:27 -0800

    Updated dialogue graphics, updated cutscene graphics

--- Assets/IntroScene/Scripts/IntroHandler.cs
+++ Assets/IntroScene/Scripts/IntroHandler.cs
@@ -7,13 +7,13 @@ using UnityEngine.SceneManagement;
 public class IntroHandler : MonoBehaviour
 {
     public Image fadeOverlay;
-    public Sprite still2;
-    public Sprite runningBackground;
+    public Sprite doorOpen;
     public SpriteRenderer sr;
-    public GameObject runningGirlPrefab;
+    AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         LeanTween.value(1, 0, 2).setOnUpdate((float value) =>
         {
             fadeOverlay.color = new Color(fadeOverlay.color.r, fadeOverlay.color.g, fadeOverlay.color.b, value);
@@ -25,17 +25,10 @@ public class IntroHandler : MonoBehaviour
 
     IEnumerator IntroSequence()
     {
-        yield return new WaitForSeconds(1);
-        //Play crashing audio
-        yield return new WaitForSeconds(2);
-        sr.sprite = still2;
-        yield return new WaitForSeconds(2);
-        sr.sprite = runningBackground;
-        sr.gameObject.transform.Translate(new Vector2(-5, 0));
-        LeanTween.moveX(sr.gameObject, sr.gameObject.transform.position.x + 10, 7);
-        GameObject runningGirl = Instantiate(runningGirlPrefab);
-        LeanTween.moveX(runningGirl, -11.5f, 5);
-        yield return new WaitForSeconds(6);
+        audioSource.Play();
+        yield return new WaitForSeconds(5.5f);
+        sr.sprite = doorOpen;
+        yield return new WaitForSeconds(4.5f);
         LeanTween.value(0, 1, 2).setOnUpdate((float value) =>
         {
             fadeOverlay.color = new Color(fadeOverlay.color.r, fadeOverlay.color.g, fadeOverlay.color.b, value);
