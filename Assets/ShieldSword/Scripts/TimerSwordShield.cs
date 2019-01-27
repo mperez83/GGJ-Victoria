095c58de8fb08cc1d26d1c45a6f264c78ac0d919
Avineesh Kompella <AvineeshKom@gmail.com>
2019-01-26 22:45:27 -0800

    made arrows and bandits faster

--- Assets/ShieldSword/Scripts/TimerSwordShield.cs
+++ Assets/ShieldSword/Scripts/TimerSwordShield.cs
@@ -7,12 +7,13 @@ using UnityEngine.SceneManagement;
 public class TimerSwordShield : MonoBehaviour
 {
     public TMP_Text m_time;
+    public GameObject player_man;
     private float time = 60f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+    
     }
 
     // Update is called once per frame
@@ -20,7 +21,7 @@ public class TimerSwordShield : MonoBehaviour
     {
         time -= Time.deltaTime;
         m_time.text = "Time: " + time.ToString("F2");
-        if (time <= 0f)
+        if (time <= 0f && player_man.GetComponent<PlayerShieldSword>().pts < 150)
         {
             SceneManager.LoadScene("LoseScreenSwordShield");
         }
