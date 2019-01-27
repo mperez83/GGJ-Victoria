d2db105bf08a76e6abf66a3857c2380fabfa92f7
Michael-C-Kirk <michael_c_kirk@yahoo.com>
2019-01-27 15:09:22 -0800

    Final Credit Scene

---
+++ Assets/Credits/CreditSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditSpawner : MonoBehaviour
+{
+    public GameObject[] ingredients;
+    private float min = 0.1f;
+    private float max = 0.50f;
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn1());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 20)
+        {
+            max = 0.4f;
+        }
+        else if (timer > 50)
+        {
+            max = 0.3f;
+            min = 0.05f;
+        }
+    }
+
+    IEnumerator Spawn1()
+    {
+        yield return new WaitForSeconds(Random.Range(min, max));
+        Instantiate(ingredients[Random.Range(0, ingredients.Length)], new Vector3(Random.Range(0f, 400f), 225, 0), Quaternion.identity);
+        StartCoroutine(Spawn2());
+    }
+
+    IEnumerator Spawn2()
+    {
+        yield return new WaitForSeconds(Random.Range(min, max));
+        Instantiate(ingredients[Random.Range(0, ingredients.Length)], new Vector3(Random.Range(0, 400f), 225, 0), Quaternion.identity);
+        StartCoroutine(Spawn1());
+    }
+}
---
+++ Assets/Credits/SceneTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTimer : MonoBehaviour
+{
+    public int maxTime = 20;
+    float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        EndCredits();
+    }
+
+    void EndCredits()
+    {
+        if(t > maxTime)
+        {
+            Debug.Log("Time!");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
